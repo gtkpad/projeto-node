09de802a14ea40675cfa0f5384136211d9299c82
Gabriel Padilha <gabrielvargaspadilha@gmail.com>
2021-08-11 17:39:37 -0300

    Basic Server and Routes file

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,15 @@
+import cors from "cors";
+import express from "express";
+
+import "dotenv/config";
+import routes from "./routes";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333);
