bd26e35f1310e8af2cd1220da9053ec6d34d3c91
Gabriel Padilha <gabrielvargaspadilha@gmail.com>
2021-08-12 09:41:20 -0300

    User Routes and data validation

--- src/modules/users/infra/http/routes/user.routes.ts
+++ src/modules/users/infra/http/routes/user.routes.ts
@@ -1,5 +1,23 @@
+import { celebrate, Segments, Joi } from "celebrate";
 import { Router } from "express";
 
+import UserController from "../controllers/UserController";
+
 const userRouter = Router();
+
+const userController = new UserController();
+
+userRouter.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().min(8).max(32).required(),
+    },
+  }),
+  userController.create
+);
+
+userRouter.get("/", userController.index);
 
 export default userRouter;
