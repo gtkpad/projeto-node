442110f82fd603bccadbf05a5e22198ccfdbc512
Gabriel Padilha <gabrielvargaspadilha@gmail.com>
2021-08-11 22:39:42 -0300

    Users module structure

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -14,6 +14,7 @@ app.use(express.json());
 
 app.use(routes);
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({
